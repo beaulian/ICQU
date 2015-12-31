54d37627ec12a3054b5f3fb57091ec31b8e53005
shenaishiren <gjw.jesus@qq.com>
2015-12-31 14:11:45 +0800

    修复发送邮件bug

--- app/graderemind/mail.py
+++ app/graderemind/mail.py
@@ -12,17 +12,18 @@ sys.setdefaultencoding("utf-8")
 
 def send_mail(content, mail_receiver):
     mail_host = "smtp.qq.com"
+    # mail_port = 465
     mail_sender = "870402916@qq.com"
+    s = des("DESCRYPT",padmode=PAD_PKCS5)
     mail_password = s.decrypt("\x94\xc6\xa8\x88\x85\x92\xe5\xdb\x85FO\x96f\xbdlE")
-    msg = MIMEText(content,_subtype="plain",_charset="utf-8")
+    msg = MIMEText(content,_subtype="html",_charset="utf-8")
     msg["Subject"] = "新成绩"
     msg["From"] = mail_sender
     msg["To"] = mail_receiver
-    try:
-        server = smtplib.SMTP()
-        server.connect(mail_host)
-        server.login(mail_sender,mail_password)
-        server.sendmail(mail_sender,mail_receiver,msg.as_string())
-        server.close()
-    except:
-        sys.exit(1)
+    
+    server = smtplib.SMTP()
+    server.connect(mail_host)
+    server.starttls()
+    server.login(mail_sender,mail_password)
+    server.sendmail(mail_sender,mail_receiver,msg.as_string())
+    server.close()
